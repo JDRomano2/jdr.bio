576fe844398b1f12182278c5f71e20350f4eda12
Joseph D. Romano <jdromano2@gmail.com>
2023-01-09 20:23:12 -0500

    Make site responsive

--- config/eleventy.config.js
+++ config/eleventy.config.js
@@ -9,8 +9,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/assets/icons/favicon.ico')
 
-  eleventyConfig.addPassthroughCopy({'src/assets/pdf/usc-talk.pdf': 'usc-talk.pdf'})
-
   return {
     templateFormats: ["html", "njk", "md", "js"],
     dir: {
